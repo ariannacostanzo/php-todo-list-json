0559f4e5ee49c778d24d4acfae71a0286e6b300c
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-22 17:40:44 +0100

    implementing creating new task

--- app/script/main.js
+++ app/script/main.js
@@ -6,35 +6,29 @@ const app = createApp({
     name: 'App',
     data() {
         return {
-            tasks: [
-                {
-                  "id": 1,
-                  "text": "HTML",
-                  "done": false
-                },
-                {
-                  "id": 2,
-                  "text": "CSS",
-                  "done": false
-                },
-                {
-                  "id": 3,
-                  "text": "Responsive design",
-                  "done": false
-                },
-                {
-                  "id": 4,
-                  "text": "Javascript",
-                  "done": false
-                }
-              ]
+            tasks: [],
+            newTask: ''
         }
     },
-    // created() {
-    //     axios.get(endpoint).then(res => {
-    //         console.log(res.data)
-    //     })
-    // }
+    methods: {
+        createNewTask() {
+            this.tasks.push(
+                {
+                    id: this.tasks.length,
+                    text: this.newTask,
+                    done: false
+                }
+            );
+            console.log(this.tasks);
+            // pulisco l'input
+            this.newTask = '';
+        }
+    },
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data
+        })
+    }
 
 });
 
