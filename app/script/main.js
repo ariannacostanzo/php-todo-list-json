87457be4d06f48184edfbb561188690157183d21
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-23 16:34:30 +0100

    centralizing fetching data

--- app/script/main.js
+++ app/script/main.js
@@ -14,24 +14,20 @@ const app = createApp({
     methods: {
         createNewTask() {
             const data = { 'task-text': this.newTaskText };
-            //la parte di creare dell'oggetto deve essere fatta da backend quindi si lascia solo task: text:this.newTask
-            const config = { headers: { 'Content-Type': 'multipart/form-data' } }
-            axios.post(endpoint, data, config).then(res => {
-                this.tasks = res.data;
-            })
+            this.fetchData(endpoint, data);
             this.newTaskText = '';
         },
         toggleTaskDone(id) {
             const data = {id};
-            const config = {headers: {'Content-Type' : 'multipart/form-data'}}
-            axios.post(`${endpoint}toggle/`, data, config).then(res => {
-                this.tasks = res.data
-            })
+            this.fetchData(`${endpoint}toggle/`, data);
         },
         deleteTask(id) {
             const data = { id };
+            this.fetchData(`${endpoint}delete/`, data);
+        },
+        fetchData(url, content) {
             const config = { headers: { 'Content-Type': 'multipart/form-data' } }
-            axios.post(`${endpoint}delete/`, data, config).then(res => {
+            axios.post(url, content, config).then(res => {
                 this.tasks = res.data
             })
         },
